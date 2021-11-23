e89d8f2a857c8076eab4d27dc6489f3d4c2f9eb9
Odysseas Vangelis <odyvangelis@gmail.com>
2021-11-23 23:08:47 +0200

    minor

--- ThesisERP/Program.cs
+++ ThesisERP/Program.cs
@@ -39,25 +39,32 @@ builder.Services.AddControllers()
                     op.SerializerSettings.ReferenceLoopHandling =
                     Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
+builder.Services.ConfigureAutoMapper();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-Log.Information("App is starting...");
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();    
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+//app.UseEndpoints(endpoints=> endpoints.MapControllers());
 
+app.MapControllers();
+Log.Information("App is starting...");
 app.Run();
